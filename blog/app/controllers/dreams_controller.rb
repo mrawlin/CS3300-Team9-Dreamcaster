c89175f91e8c3b6bbb8df961f999d3a70be9cc3c
Ensign <ensign@workstation.liddev.com>
2016-03-12 16:17:42 -0700

    Created links to each dream

--- blog/app/controllers/dreams_controller.rb
+++ blog/app/controllers/dreams_controller.rb
@@ -8,15 +8,40 @@ class DreamsController < ApplicationController
 		@dreams = Dream.all
 	end
 
+	def edit
+		@dream = Dream.find(params[:id])
+	end
+
+	def update
+		@dream = Dream.find(params[:id])
+
+		if @dream.update(dream_params)
+			redirect_to @dream
+		else
+			render 'edit'
+		end
+	end
+
   def new
+		@dream = Dream.new
   end
 
   def create
     @dream = Dream.new(dream_params)
 
-    @dream.save
-    redirect_to @dream
-  end
+    if @dream.save
+			redirect_to @dream
+		else
+			render 'new'
+		end
+	end
+
+	def destroy
+		@dream= Dream.find(params[:id])
+		@dream.destroy
+
+		redirect_to dreams_path
+	end
 
   private
   	def dream_params
