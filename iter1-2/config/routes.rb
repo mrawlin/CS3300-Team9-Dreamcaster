6aaefaa37babd1132b6e00d9cc4ecfb9681c2b1d
Austin Wasinger <awasing3@uccs.edu>
2016-03-11 17:45:18 -0700

    Fixing iter1-2

--- iter1-2/config/routes.rb
+++ iter1-2/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'dreamcasters/newpost'
+
+  get 'dreamcasters/newpost'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
