95b829c0c46ce1b35a7e716940390d70ddc358a9
Austin Wasinger <awasing3@uccs.edu>
2016-03-12 03:38:50 -0700

    added heroku and stuff

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160312021217) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "dreams", force: :cascade do |t|
     t.text     "title"
     t.text     "text"
