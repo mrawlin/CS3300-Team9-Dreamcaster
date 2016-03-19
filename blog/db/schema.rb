357cc3f6d9160cdb8e50bcbf51e2124189ffcbf1
Ensign <ensign@workstation.liddev.com>
2016-03-19 16:29:05 -0600

    push

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -40,6 +40,16 @@ ActiveRecord::Schema.define(version: 20160318204325) do
     t.datetime "updated_at"
   end
 
+  create_table "projects", force: :cascade do |t|
+    t.string   "title"
+    t.string   "user"
+    t.text     "description"
+    t.datetime "due_date"
+    t.text     "extended_description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "rates", force: :cascade do |t|
     t.integer  "rater_id"
     t.integer  "rateable_id"
