222e563452d5802c6372f22246c43acec52c2761
Austin Wasinger <awasing3@uccs.edu>
2016-03-19 15:28:28 -0600

    adding framework for user model

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   post '/rate' => 'rater#create', :as => 'rate'
   get 'welcome/index'
 
---
+++ blog/spec/controllers/users_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+end
