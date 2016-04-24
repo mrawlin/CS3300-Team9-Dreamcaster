7dcc49093da9ce6057f7d7ccf4805ddb196f8e8c
hgentry <hgentry@uccs.edu>
2016-04-23 18:11:57 -0600

    Started subthreading

---
+++ blog/app/controllers/subthreads_controller.rb
@@ -0,0 +1,19 @@
+class SubthreadsController < ApplicationController
+    def create
+      @comment = comment.find_by(params[:commment_id])
+      @subthread = comment.subthreads.create(comment_params)
+      redirect_to dream_path(@comment)
+    end
+
+    def destroy
+      @comment = Comment.find(params[:comment_id])
+      @subthread = @comment.find(params[:id])
+      @subthread.destroy
+      redirect_to dream_path(@comment)
+    end
+
+    private
+    def comment_params
+      params.require(:subthread).permit(:commenter, :body)
+    end
+end
---
+++ blog/app/models/subthread.rb
@@ -0,0 +1,6 @@
+class Subthread < ActiveRecord::Base
+  belongs_to :comment
+  default_scope -> { order(created_at: :desc) }
+  validates :comment_id, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
+end
