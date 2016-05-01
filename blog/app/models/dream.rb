dcd7fbc5e848994e7c4b0748ce09106f4b75f9cc
Andreas Grell <agrell@uccs.edu>
2016-04-30 19:26:40 -0600

    removed th line about validating users

--- blog/app/models/dream.rb
+++ blog/app/models/dream.rb
@@ -2,7 +2,6 @@ class Dream < ActiveRecord::Base
 
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
-  #validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 	validates :title, presence: true,
                     length:{minimum: 5 }
