35ac1af1d5f0bc7f070e389758f3792db19a300f
Andreas Grell <agrell@uccs.edu>
2016-04-30 19:09:57 -0600

    disabled the editing dreams if you are not the same user

--- blog/app/controllers/dreams_controller.rb
+++ blog/app/controllers/dreams_controller.rb
@@ -66,7 +66,11 @@ class DreamsController < ApplicationController
   end
 
 	def create
-		@dream = current_user.dreams.create!(dream_params) #Dream.create(dream_params)
+		if logged_in?
+			@dream = current_user.dreams.create!(dream_params) #Dream.create(dream_params)
+		else 
+			@dream = Dream.create(dream_params)
+		end
 
 		if @dream.valid? then
 			redirect_to @dream
