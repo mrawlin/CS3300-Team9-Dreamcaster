973466ee418f3ccb0c72dfc6cc91e122af14cd54
Austin Wasinger <awasing3@uccs.edu>
2016-05-01 23:49:58 -0600

    fixing tests

--- blog/db/seeds.rb
+++ blog/db/seeds.rb
@@ -29,5 +29,6 @@ require 'faker'
 users = User.order(:created_at).take(6)
 50.times do
  content = Faker::Lorem.sentence(5)
- users.each { |user| user.microposts.create!(content: content) }
+ title = Faker::Lorem.sentence(20)
+ users.each { |user| user.dreams.create!(title: title, content: content) }
 end
