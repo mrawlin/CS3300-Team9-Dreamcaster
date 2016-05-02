c4ace84e402a7399ffe8b8ee9d91c9a40616ddea
Austin Wasinger <awasing3@uccs.edu>
2016-05-01 23:54:30 -0600

    Fixed search bar thing. text whatever

--- blog/db/seeds.rb
+++ blog/db/seeds.rb
@@ -8,27 +8,27 @@
 
 
 #The following code makes 6 sample users each with 50 posts. So that we may see how the microposts are manages on their pages
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
-             admin: true)
+# User.create!(name:  "Example User",
+#              email: "example@railstutorial.org",
+#              password:              "foobar",
+#              password_confirmation: "foobar",
+#              admin: true)
 
 
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
-end
+# 99.times do |n|
+#   name  = Faker::Name.name
+#   email = "example-#{n+1}@railstutorial.org"
+#   password = "password"
+#   User.create!(name:  name,
+#                email: email,
+#                password:              password,
+#                password_confirmation: password)
+# end
 
-require 'faker'
-users = User.order(:created_at).take(6)
-50.times do
- content = Faker::Lorem.sentence(5)
- title = Faker::Lorem.sentence(20)
- users.each { |user| user.dreams.create!(title: title, content: content) }
-end
+# require 'faker'
+# users = User.order(:created_at).take(6)
+# 50.times do
+#  content = Faker::Lorem.sentence(5)
+#  title = Faker::Lorem.sentence(20)
+#  users.each { |user| user.dreams.create!(title: title, content: content) }
+# end
